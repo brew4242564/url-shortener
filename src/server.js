6a8b90b246235943fe5fdeb657f0e10e709e87c1
Damian <vera.damian@proton.me>
2026-08-14 19:40:18 -0300

    feat: add basic get function to redirect short links.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import { supabase } from './services/supabase';
-
+import { supabase } from './services/supabase.js';
 const app = express();
 
+app.get('/:code', async(req, res)=> {
+    const {code} = req.params;
+    const {data, error} = await supabase.from("URL_SHORTENER").select('link').eq('id', code).single();
+    if(error || !data){
+        return res.status(404).send('URL no encontrada');
+    }
+    res.redirect(data.link);
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
     console.log(`Listening port: ${PORT}`)
-})
+}) 
