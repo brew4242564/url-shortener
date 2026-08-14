c83f7a4422b3d08b5fbbdf04ce2b372f7e0f9b80
Damian <vera.damian@proton.me>
2026-08-14 19:52:45 -0300

    change sucess message

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import { supabase } from './services/supabase.js';
 import { nanoid } from 'nanoid';
 
+const BASE_URL = process.env.BASE_URL
 const createUrl = async (url) => {
     if (!url) {
         console.error("You must enter a URL");
@@ -10,17 +11,18 @@ const createUrl = async (url) => {
     while (!inserted) {
         let code = nanoid(7);
         const { error } = await supabase.from("URL_SHORTENER").insert([{ link: url, id: code, }])
-        if (!error) inserted = true;
+        if (!error){
+            inserted = true;
+            console.log(`${BASE_URL}${code}`);
+        } 
         // error 23505 is duplicate value in primary key or value with unique constraint.
         if (error && error.code !== '23505') {
             console.error("Error on createURL: ", error.message);
             return;
         }
     }
-    console.log('Sucess');
 }
 
 const urlInput = process.argv[2];
-
 createUrl(urlInput);
 
