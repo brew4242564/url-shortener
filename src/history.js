112727b51d16e68ca57d57df4378646dc7c34fc6
Damian <vera.damian@proton.me>
2026-08-15 14:21:04 -0300

    add cli-table3 for a better data visualitation.

--- src/history.js
+++ src/history.js
@@ -1,5 +1,12 @@
+import Table from "cli-table3";
 import { supabase } from "./services/supabase.js"
+
 const BASE_URL = process.env.BASE_URL;
+
+const table = new Table({
+    head:['Short Link', 'Link', 'Created at'],
+})
+
 const history = async () => {
     const { data, error } = await supabase.from("URL_SHORTENER").select('id, link, created_at');
     if (error) {
@@ -8,8 +15,9 @@ const history = async () => {
     }
 
     data.forEach(row => {
-        console.log(`${BASE_URL}${row.id} → ${row.link} → ${row.created_at}`)
+        table.push([`${BASE_URL}${row.id}`, row.link, row.created_at]);
     })
+    console.log(table.toString());
 }
 
 history();
