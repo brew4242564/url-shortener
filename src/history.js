d79372ebc63f93aa69eb568f2f95620e4b4387da
Damian <vera.damian@proton.me>
2026-08-15 14:42:46 -0300

    add pagination

--- src/history.js
+++ src/history.js
@@ -7,8 +7,14 @@ const table = new Table({
     head:['Short Link', 'Link', 'Created at'],
 })
 
-const history = async () => {
-    const { data, error } = await supabase.from("URL_SHORTENER").select('id, link, created_at');
+const PAGES = 20;
+const history = async (page=0) => {
+    const from = page * PAGES;
+    const to = from + PAGES - 1;
+    const { data, error, count } = await supabase.from("URL_SHORTENER")
+    .select('id, link, created_at', {count: 'exact'})
+    .order('created_at', {ascending: false})
+    .range(from,to);
     if (error) {
         console.error("Error fetching history", error.message);
         return;
@@ -18,6 +24,7 @@ const history = async () => {
         table.push([`${BASE_URL}${row.id}`, row.link, row.created_at]);
     })
     console.log(table.toString());
+    console.log(`Página ${page + 1} — mostrando ${data.length} de ${count} links totales`);
 }
 
 history();
